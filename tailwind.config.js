d6962a331c1ad779cf99a954ae80e888fbdbf90a
Shoeboom <shubhamb1810@gmail.com>
2020-09-25 18:30:55 -0400

    setup tailwind auto compleate

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	purge: [
+		"./src/components/**/*.{js,ts,jsx,tsx}",
+		"./src/pages/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				navbar: "#EEEEEE",
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+};
